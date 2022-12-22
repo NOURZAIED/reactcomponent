f79a1ab30edf0416a3e03fefc68295628bf61ee7
NOURZAIED <zaiednour7@gmail.com>
2022-12-22 11:06:04 +0100

    first commit

---
+++ src/profile/fullname.js
@@ -0,0 +1,12 @@
+function Name () {
+    return (
+        <div>
+        <p className="hello">
+            Hello, my name is ZAIED Nour and I am writing to introduce myself. My interests include coding, art, and music .
+        In the past i used to work at a geomatic company, after that I choose to study web development Because most businesses require a full-stack Web developer for a variety of reasons. Web developers who are full-stack can work with a variety of technologies ,so I made some research on Gomycode . 
+        and finally I started practicing to code in Gomycode with HTML,CSS Basics and developed my simple portfolio as one my beginners project containing my basic personal details .
+        </p>
+    </div>
+    )
+}
+export default Name;
---
+++ src/profile/profilphoto.js
@@ -0,0 +1,11 @@
+import user from '../user.png';
+
+function Photo () {
+    return (
+    <div>
+        <img src= {user} alt='this is a user profile img' />
+    </div>
+    )
+}
+
+export default Photo;
